23ff5b0978378dcb09331b46e4811f389a591579
Banich <55958209+Banich-AI@users.noreply.github.com>
2023-03-06 10:55:36 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,14 @@
+# Объект сотрудник
+
+job_title = "Тренер"
+gender = "Мужчина"
+age = 35
+on_job = True
+nps = 8.2
+best_job = 'Декомпозиция'
+
+print((type(job_title)))
+print((type(gender)))
+print((type(age)))
+print((type(on_job)))
+print((type(best_job)))
